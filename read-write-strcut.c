b2dcb59654927165aaf5856a2dd846712d35cc7c
Thippanna Hongal <thongal@thongal-000608.local>
2020-06-27 19:21:39 -0700

    Add evpn notes, Add few more programs.

---
+++ read-write-strcut.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct person {
+char name[10];
+int  id;
+} p_t;
+
+int main(void) {
+  FILE *out, *data_fp= fopen("person.dat", "w");
+
+  if(data_fp == NULL){
+    printf("Not able to open file");
+    return 0;
+  }
+  p_t p1={"Ram",10};
+  p_t p2={"Sham",2};
+  p_t pr;;
+
+  fwrite(&p1, sizeof(struct person), 1, data_fp);
+  fwrite(&p2, sizeof(struct person), 1, data_fp);
+  printf("Done Writing\n");
+  fclose(data_fp);
+
+  /* Open and read now */
+  out = fopen("person.dat", "r");
+  if(!out){
+    printf("Failed to open fileout");
+    return 0;
+  }
+  while(fread (&pr, sizeof(struct person), 1, out)){
+    printf("name %s, id %d\n",pr.name, pr.id);
+  }
+  fclose(out);
+
+}
